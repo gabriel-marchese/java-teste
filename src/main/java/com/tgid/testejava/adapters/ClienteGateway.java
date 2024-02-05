32a54c9a3cb4936c3cde0b791fc36e45162e3e09
Gabriel M <gabriel.marchese2@gmail.com>
2024-02-05 20:02:11 -0300

    Feat: Retirando arquivos testes

--- src/main/java/com/tgid/testejava/adapters/ClienteGateway.java
+++
@@ -1,6 +0,0 @@
-package com.tgid.testejava.adapters;
-
-public interface ClienteGateway {
-    void doDeposit(String CNPJ, Integer valor);
-    void doWithdraw(String CNPJ, Integer valor);
-}
--- src/main/java/com/tgid/testejava/core/cases/ClienteUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.tgid.testejava.core.cases;
-
-import java.math.BigDecimal;
-
-public interface ClienteUseCase {
-    void realizarDeposito(String CNPJ, BigDecimal valor);
-    void realizarSaque(String CNPJ, BigDecimal valor);
-}
